42f8e98f1079f2b9004d4db0cc34d812373fc0f6
425776024 <425776024@qq.com>
2020-06-21 13:22:06 +0800

    init

--- codemirror_python/index.js
+++ codemirror_python/index.js
@@ -1,12 +1,12 @@
-function mdSwitch() {
-        var mdValue = document.getElementById("md_v").innerText;
-        var converter = new showdown.Converter();
-        var html = converter.makeHtml(mdValue);
-        document.getElementById("show-area").innerHTML = html;
-};
+//function mdSwitch() {
+//        var mdValue = document.getElementById("md_v").innerText;
+//        var converter = new showdown.Converter();
+//        var html = converter.makeHtml(mdValue);
+//        document.getElementById("show-area").innerHTML = html;
+//};
 
 window.onload = function () {
-    mdSwitch();
+//    mdSwitch();
     var el = document.getElementById("editor");
 	var version = "# version: Python3\n";
 	var codeAreaTip = "# Edit your code here:\n \n";
@@ -33,35 +33,10 @@ window.onload = function () {
         myCodeMirror.showHint(); // 注意，注释了CodeMirror库中show-hint.js第131行的代码（阻止了代码补全，同时提供智能提示）
     });
     var run_btn = document.getElementById("run_btn");
-    var save_btn = document.getElementById("save_btn");
-    var edit_md = document.getElementById("edit_md");
-    edit_md.onclick = function() {
-        var name_v = document.getElementById("code_name").innerText;
-        host = window.location.host;
-        url='http://'+host+'/md/'+name_v;
-        window.open(url);
-    };
+
     run_btn.onclick = function() {
         var value = myCodeMirror.getValue();
         runit(value);
-
-    };
-    save_btn.onclick = function() {
-        var value = myCodeMirror.getValue();
-        var name_v = document.getElementById("code_name").innerText;
-//        alert(name_v);
-        axios.put('/save', {
-                    "code_str":value,
-                    "name":name_v,
-                  })
-                  .then(function (response) {
-<!--                    host = window.location.host;-->
-<!--                    url='http://'+host+'/code/'+response.data['name'];-->
-<!--                    window.location.href = url;-->
-                  })
-                  .catch(function (error) {
-                    console.log(error);
-                  });
 
     };
 };
